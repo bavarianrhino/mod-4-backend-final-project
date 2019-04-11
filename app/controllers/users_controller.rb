a02dfa9d1b0e840de7882e6e08e8db50d4590788
spikeburton <ralphburton3@gmail.com>
2019-04-11 13:59:20 -0400

    add json methods for user to return meaningful data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+    render json: @users.user_json, status: :ok
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user.user_json, status: :ok
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,22 @@ class User < ApplicationRecord
   has_secure_password
   has_many :cars
   has_many :scores
+
+  def user_json
+    {
+      id: self.id,
+      username: self.username,
+      high_score: self.high_score,
+      cars: self.cars
+    }
+  end
+
+  def high_score
+    self.scores.collect(&:points).max
+    # self.scores.collect { |u| u.points }
+  end
+
+  def self.user_json
+    User.all.collect { |u| u.user_json }
+  end
 end
