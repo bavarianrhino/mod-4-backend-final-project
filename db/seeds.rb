ed04c90e38c18d54fb802f04f0476d0f303dfa9c
github username <github email address>
2019-04-16 14:17:30 -0400

    new seeded cars

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,21 +13,31 @@ u2 = User.create(username: "spike", password: "$ALT")
 u3 = User.create(username: "dana", password: "$ALT")
 u4 = User.create(username: "brian", password: "$ALT")
 
-Car.create(name: "Red Car",
-    up: "http://images.clipartpanda.com/car-top-view-clipart-red-racing-car-top-view-fe3a.png",
-    down: "http://images.clipartpanda.com/car-top-view-clipart-red-racing-car-top-view-fe3a.png",
-    left: "http://images.clipartpanda.com/car-top-view-clipart-red-racing-car-top-view-fe3a.png",
-    right: "http://images.clipartpanda.com/car-top-view-clipart-red-racing-car-top-view-fe3a.png",
+Car.create(name: "Black Vintage",
+    up: '/black-vintage/black-vintage-up.png',
+    down: '/black-vintage/black-vintage-down.png',
+    left: '/black-vintage/black-vintage-left.png',
+    right: '/black-vintage/black-vintage-right.png',
     max_fuel: 80,
     tread_wear: 65,
     health: 75,
     user_id: 1)
 
-Car.create(name: "Black Car",
-    up: "http://www.sclance.com/pngs/car-top-view-png/car_top_view_png_219321.png",
-    down: "http://www.sclance.com/pngs/car-top-view-png/car_top_view_png_219321.png",
-    left: "http://www.sclance.com/pngs/car-top-view-png/car_top_view_png_219321.png",
-    right: "http://www.sclance.com/pngs/car-top-view-png/car_top_view_png_219321.png",
+Car.create(name: "Blue Police",
+    up: '/blue-police/blue-police-up.png',
+    down: '/blue-police/blue-police-down.png',
+    left: '/blue-police/blue-police-left.png',
+    right: '/blue-police/blue-police-right.png',
+    max_fuel: 70,
+    tread_wear: 75,
+    health: 85,
+    user_id: 1)
+
+Car.create(name: "Black Modern",
+    up: '/black-modern/black-modern-up.png',
+    down: '/black-modern/black-modern-down.png',
+    left: '/black-modern/black-modern-left.png',
+    right: '/black-modern/black-modern-right.png',
     max_fuel: 90,
     tread_wear: 55,
     health: 70,
@@ -43,23 +53,6 @@ Car.create(name: "Blue Bugatti",
     health: 80,
     user_id: 2)
 
-Car.create(name: "Black Modern",
-    up: '/black-modern/black-modern-up.png',
-    down: '/black-modern/black-modern-down.png',
-    left: '/black-modern/black-modern-left.png',
-    right: '/black-modern/black-modern-right.png',
-    max_fuel: 90,
-    tread_wear: 55,
-    health: 85)
-
-Car.create(name: "Black Vintage",
-    up: '/black-vintage/black-vintage-up.png',
-    down: '/black-vintage/black-vintage-down.png',
-    left: '/black-vintage/black-vintage-left.png',
-    right: '/black-vintage/black-vintage-right.png',
-    max_fuel: 50,
-    tread_wear: 95,
-    health: 75)
 
 
 
